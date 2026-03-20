88770f282017744fd498996e827c1a9de9e44a0a
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-03-20 12:02:13 +0200

    update routes for testing

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import authRoutes from "./routes/auth";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 
