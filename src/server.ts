f982a3aff539d59f7cae220d7a2cdd02d776fbac
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-03-20 13:48:45 +0200

    update server

--- src/server.ts
+++ src/server.ts
@@ -4,31 +4,28 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth";
 
-
 dotenv.config();
 
 const app = express();
 
-export default app;
-
-if (require.main === module) {
-    const PORT =process.env.PORT || 3000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}` ));
-}
-
-
+  //midleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 
+
+// route -----
 app.use("/api/auth", authRoutes);
 
-
+// ---- endpoint------
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// if running directly
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app; 
